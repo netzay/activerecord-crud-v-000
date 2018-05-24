10ab3cc93fa5cae9e8d32b5a6b81871f7f85976d
Ally Netzel <netzelally@gmail.com>
2018-05-24 01:02:05 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,14 +42,14 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.first.id
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title:, release_date:,director:)
 end
 
 def can_find_using_where_clause_and_be_sorted
