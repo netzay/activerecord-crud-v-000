c88fffea55cdcb9d7e4a6dd5b75cc09d4ba06fac
Ally Netzel <netzelally@gmail.com>
2018-05-24 02:02:13 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,7 +84,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   Movie.destroy(1)
-  __
+  Movie
 end
 
 def can_destroy_all_items_at_once
