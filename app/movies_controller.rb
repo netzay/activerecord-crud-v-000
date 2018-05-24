5a8143fa2af51f7df2e619b01a91df4456abd243
Ally Netzel <netzelally@gmail.com>
2018-05-24 01:06:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -51,6 +51,7 @@ def can_find_by_multiple_attributes
   # director == "Me"
   Movie.find_by(title:)
 end
+end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
